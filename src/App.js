c405f3448d6303ca1ed025c6602e16e8aaa981da
Hirzan Alhakim <hirzan.hakim@ralali.com>
2022-08-21 13:51:31 +0700

    Hide Home Pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
 import Example from './pages/Example'
+// import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/example" element={<Example />} />
+        {/* <Route path="/" element={<Example />} /> */}
+        <Route path="/table-filter" element={<Example />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
