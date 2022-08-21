7d8b24618599ec0c5fcabfc002d8f3f01e2e6419
Hirzan Alhakim <hirzan.hakim@ralali.com>
2022-08-21 13:52:19 +0700

    update link Home

--- src/components/BreadCrumb/index.js
+++ src/components/BreadCrumb/index.js
@@ -1,15 +1,14 @@
-
-import Typography from '@mui/material/Typography';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography'
+import Breadcrumbs from '@mui/material/Breadcrumbs'
+import Link from '@mui/material/Link'
 
 export default function BasicBreadcrumbs({ title }) {
   return (
     <Breadcrumbs aria-label="breadcrumb">
-      <Link underline="hover" color="inherit" href="/">
+      <Link underline="hover" color="inherit" href="/table-filter">
         Home
       </Link>
       <Typography color="text.primary">{title}</Typography>
     </Breadcrumbs>
-  );
+  )
 }
