27eebab2becb5ccf7013c1961efe07555d1ed2f9
Hirzan Alhakim <hirzan.hakim@ralali.com>
2022-08-21 13:51:47 +0700

    update Test NotFound Page

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test("renders Example Project", () => {
-  render(<App />);
-  const exampleElement = screen.getByText(/Example Project/i);
-  const linkElement = screen.getByText(/Go To Table/i);
-  expect(exampleElement).toBeInTheDocument();
-  expect(linkElement).toBeInTheDocument();
-});
+test('will renders NotFound Project', () => {
+  render(<App />)
+  const exampleElement = screen.getByText(/Page Not Found/i)
+  expect(exampleElement).toBeInTheDocument()
+})
