eea699ddb9163894a3599fe92673290fea989177
Hirzan Alhakim <hirzan.hakim@ralali.com>
2022-08-21 14:17:41 +0700

    fixing and clearing

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('will renders NotFound Project', () => {
-  render(<App />)
-  const exampleElement = screen.getByText(/Page Not Found/i)
-  expect(exampleElement).toBeInTheDocument()
-})
